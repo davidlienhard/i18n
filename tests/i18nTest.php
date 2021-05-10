5b06d4ec4a399b0a4e708047a492497a98a6e194
David Lienhard <david.lienhard@tourasia.ch>
2021-05-10 15:29:50 +0200

    add basic unit-tests add basic test to check if instance can be created

---
+++ tests/i18nTest.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DavidLienhard;
+
+use DavidLienhard\i18n\i18n;
+use DavidLienhard\i18n\i18nInterface;
+use PHPUnit\Framework\TestCase;
+
+class i18nTest extends TestCase
+{
+    /** @covers \DavidLienhard\Config\Config */
+    public function testCanBeCreated(): void
+    {
+        $config = new i18n();
+        $this->assertInstanceOf(i18n::class, $config);
+        $this->assertInstanceOf(i18nInterface::class, $config);
+    }
+}
